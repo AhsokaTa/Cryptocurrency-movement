65547ef3b0e6b3d84c3814f758b688960c9fc534
Ahsoka_Ta <sanchz.cris@gmail.com>
2023-05-13 22:49:46 +0200

    route to index.html, initial html structure, add pico.css , add styles.css

--- cripto_move/routes.py
+++ cripto_move/routes.py
@@ -1,5 +1,8 @@
 from cripto_move import app
+from flask import render_template
+
 
 @app.route("/")
 def inde():
-    return "Flask Skeleton"
+    #return "Flask Skeleton"
+    return render_template("index.html")
