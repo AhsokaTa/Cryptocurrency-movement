6279ff3b9f228a7cd9e0b870e27d3e4080fde249
Ahsoka_Ta <sanchz.cris@gmail.com>
2023-05-14 13:09:48 +0200

    Add Jinja template, add purchase.html file, add status.html

--- cripto_move/routes.py
+++ cripto_move/routes.py
@@ -3,6 +3,25 @@ from flask import render_template
 
 
 @app.route("/")
-def inde():
-    #return "Flask Skeleton"
-    return render_template("index.html")
+def index():
+    JSONstructure = [
+    {
+    "id": '1',
+    "date": 'date field',
+    "time": 'time field',
+    "from": 'from field',
+    "q": 'q field',
+    "to": 'to field',
+    "q1": 'q1 field',
+    "PU": 'P.U field'
+    } 
+      ]  
+    return render_template("index.html", data = JSONstructure)
+
+@app.route("/purchase")
+def purchase():
+    return render_template("purchase.html")
+
+@app.route("/status")
+def status():
+    return render_template("status.html")
